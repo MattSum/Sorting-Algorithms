306bcd413944db44e57d093ed462fc0e16085898
Mateusz <msumiec@gmail.com>
2019-08-08 22:14:07 +0200

    initialize script

---
+++ quicksort.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Quick sort algorithm implemented in Python3
+"""
+
+
+def quick_sort(data: list, beg: int, end: int) -> list:
+    pass
+
+
+def partition(data: list, beg: int, end: int) -> list:
+    pass
+
+
+if __name__ == "__main__":
+    arr = [51, 23, 56, 19, 4]
+    quick_sort(arr, 0, len(arr)-1)
+    print(arr)
