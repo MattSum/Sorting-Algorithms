1e0691fbb4f34b21f0765ad69c3e032089323308
Mateusz <msumiec@gmail.com>
2019-08-08 22:15:59 +0200

    Algoritm works, program works, everything works

--- quicksort.py
+++ quicksort.py
@@ -5,11 +5,21 @@ Quick sort algorithm implemented in Python3
 
 
 def quick_sort(data: list, beg: int, end: int) -> list:
-    pass
+    if beg < end:
+        p = partition(data, beg, end)
+        quick_sort(data, beg, p-1)
+        quick_sort(data, p+1, end)
 
 
 def partition(data: list, beg: int, end: int) -> list:
-    pass
+    pivot = data[end]
+    i = beg
+    for j in range(beg, end):
+        if data[j] < pivot:
+            data[i], data[j] = data[j], data[i]
+            i = i + 1
+    data[i], data[end] = data[end], data[i]
+    return i
 
 
 if __name__ == "__main__":
