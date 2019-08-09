bb17f06ce2a7ea76a9f396014a24b6023dc0632c
Mateusz <msumiec@gmail.com>
2019-08-09 21:10:07 +0200

    Function selection_sort working properly

--- selectionsort.py
+++ selectionsort.py
@@ -5,6 +5,22 @@ Implementation of selection sorting algotithm in Python
 Reference: https://en.wikipedia.org/wiki/Selection_sort
 """
 
+def selection_sort(A: list) -> None:
+    for i in range(len(A)):
+        minimal = i
 
-def selection_sort(A):
-    pass
+        for j in range(i+1, len(A)):
+            if A[j] < A[minimal]:
+                minimal = j
+    
+        if minimal != i:
+            A[i], A[minimal] = A[minimal], A[i]
+    
+        return None
+
+
+if __name__ == "__main__":
+    arr = [51, 23, 56, 19, 4]
+    print(arr)
+    selection_sort(arr)
+    print(arr)
