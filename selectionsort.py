c982f14fed1c16264e0175a4202213a7556c67c5
Mateusz <msumiec@gmail.com>
2019-08-09 20:58:22 +0200

    Initialize with selectionsort script

---
+++ selectionsort.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Implementation of selection sorting algotithm in Python
+Reference: https://en.wikipedia.org/wiki/Selection_sort
+"""
+
+
+def selection_sort(A):
+    pass
