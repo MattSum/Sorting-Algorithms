17712d1afaad6259ab09eb7fe2f045c1425e46de
Mateusz <msumiec@gmail.com>
2019-08-12 20:27:48 +0200

    It's shufflin

--- fisher_yates_shuffle.py
+++ fisher_yates_shuffle.py
@@ -5,5 +5,13 @@ Implementation of Fisher Yates shuffle algorithm also known as Knuth shuffle
 import random
 
 
-def fisher_yates_shuffle(A: list) -> list:
-      pass
+def fisher_yates_shuffle(A: list) -> None:
+      for i in range(len(A)-1, 0, -1):
+            j = random.randint(0, i)
+            A[j], A[i] = A[i], A[j]
+
+
+if __name__ == '__main__':
+      A = list(range(40))
+      fisher_yates_shuffle(A)
+      print(A)
