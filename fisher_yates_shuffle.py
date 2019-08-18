30ee7c5f0dd43e3f500d2b0825ef2ecfb8f3423c
Mateusz <msumiec@gmail.com>
2019-08-18 22:00:30 +0200

    Add visualisation of shuffling algorithm

--- fisher_yates_shuffle.py
+++ fisher_yates_shuffle.py
@@ -3,15 +3,38 @@ Implementation of Fisher Yates shuffle algorithm also known as Knuth shuffle
 
 """
 import random
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
 def fisher_yates_shuffle(A: list) -> None:
       for i in range(len(A)-1, 0, -1):
             j = random.randint(0, i)
             A[j], A[i] = A[i], A[j]
+            yield A
+
+
+def draw():
+      plt.style.use('dark_background')
+      fig, ax = plt.subplots()
+      scatter_points = ax.bar(A, range(len(A)))
+      text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+      iteration = [0]
+      def update_fig(A, points, iteration):
+            for points, val in zip(points, A):
+                  points.set_height(val)
+            iteration[0] += 1
+            text.set_text("# of operations: {}".format(iteration[0]))
+
+      ani = FuncAnimation(fig, 
+                          func=update_fig, 
+                          fargs=(scatter_points, iteration), 
+                          frames=fisher_yates_shuffle(A), 
+                          interval=3,
+                          repeat=False)
+      plt.show()
 
 
 if __name__ == '__main__':
       A = list(range(40))
-      fisher_yates_shuffle(A)
-      print(A)
+      draw()
