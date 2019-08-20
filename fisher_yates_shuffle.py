81d011b3e876c361cb6641fa122a169b4718ac8c
Mateusz <msumiec@gmail.com>
2019-08-20 19:20:48 +0200

    Correct indentation(updated IDE)

--- fisher_yates_shuffle.py
+++ fisher_yates_shuffle.py
@@ -1,6 +1,6 @@
 """
-Implementation of Fisher Yates shuffle algorithm also known as Knuth shuffle
-
+Implementation and visualisation with barplots of 
+Fisher Yates shuffle algorithm also known as Knuth shuffle.
 """
 import random
 import matplotlib.pyplot as plt
@@ -8,33 +8,35 @@ from matplotlib.animation import FuncAnimation
 
 
 def fisher_yates_shuffle(A: list) -> None:
-      for i in range(len(A)-1, 0, -1):
-            j = random.randint(0, i)
-            A[j], A[i] = A[i], A[j]
-            yield A
+    """Created as generator for animation"""
+    for i in range(len(A)-1, 0, -1):
+        j = random.randint(0, i)
+        A[j], A[i] = A[i], A[j]
+        yield A
+
 
 def draw():
-      plt.style.use('dark_background')
-      fig, ax = plt.subplots()
-      bars = ax.bar(A, range(len(A)))
-      text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots()
+    bars = ax.bar(A, range(len(A)))
+    text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
 
-      iteration = [0]
-      def update(A, points, iteration):
-            for points, val in zip(points, A):
-                  points.set_height(val)
-            iteration[0] += 1
-            text.set_text(f"Number of operations: {iteration[0]}")
+    iteration = [0]
+    def update(A, points, iteration):
+        for points, val in zip(points, A):
+            points.set_height(val)
+        iteration[0] += 1
+        text.set_text(f"Number of operations: {iteration[0]}")
 
-      ani = FuncAnimation(fig, 
-                          func=update, 
-                          fargs=(bars, iteration), 
-                          frames=fisher_yates_shuffle(A), 
-                          interval=3,
-                          repeat=False)
-      plt.show()
+    ani = FuncAnimation(fig, 
+                        func=update, 
+                        fargs=(bars, iteration), 
+                        frames=fisher_yates_shuffle(A), 
+                        interval=3,
+                        repeat=False)
+    plt.show()
 
 
 if __name__ == '__main__':
-      A = list(range(80))
-      draw()
+    A = list(range(80))
+    draw()
